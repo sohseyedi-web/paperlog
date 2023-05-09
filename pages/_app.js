09ab476662d05d93d715e8232b2f66d5b2fe7b4e
sohseyedi <sohseyedi@gmail.com>
2023-05-09 10:57:46 +0330

    bug fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,16 @@
 import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <SessionProvider session={session}>
-        <Navbar />
-        <Component {...pageProps} />
-        <Toaster position="top-center" />
-      </SessionProvider>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster position="top-center" />
     </QueryClientProvider>
   );
 }
